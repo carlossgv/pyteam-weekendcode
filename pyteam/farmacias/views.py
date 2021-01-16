1ce7a817146f102d176bd419c31a65772a9c21da
GTXs <arayabarrioseduardo@gmail.com>
2021-01-16 19:48:39 -0300

    Devolucion de las farmacias cercanas por lat y lng del user

--- pyteam/farmacias/views.py
+++ pyteam/farmacias/views.py
@@ -1,6 +1,6 @@
 from django.views.generic.base import TemplateView
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
 from .utils import devuelve_farmacias_cercanas
 
 # Create your views here.
@@ -8,21 +8,21 @@ from .utils import devuelve_farmacias_cercanas
 class TestView(TemplateView):
     template_name = "farmacias/test.html"
 
-def encuentra_farmacias_cercanas(request, usuario_latitud = -32.878131, usuario_longitud = -71.244828):
+def encuentra_farmacias_cercanas(request, usuario_latitud = None, usuario_longitud = None):
+
+    if usuario_latitud == None or usuario_longitud == None:
+        raise Http404("Algo ha fallado con respecto a la latitud o longitud de la ubicacion del usuario")
+
+    usuario_latitud = float(usuario_latitud)
+    usuario_longitud = float(usuario_longitud)
 
     url = "https://farmanet.minsal.cl/maps/index.php/ws/getLocalesTurnos"
     
     farmacias_cercanas = devuelve_farmacias_cercanas(usuario_latitud, usuario_longitud, url)
-    #print(farmacias_cercanas)
-    json_response = []
+    
     if farmacias_cercanas:
-        for farmacia in farmacias_cercanas:
-            json_response.append({
-                'id': farmacia['local_id'],
-                'latitud':farmacia['local_lat'], 
-                'longitud':farmacia['local_lng'], 
-                'nombre':farmacia['local_nombre']
-                })
+        return JsonResponse(farmacias_cercanas, safe=False)        
     else:
-        return JsonResponse([{'id':'-1'}], safe=False) # En caso de no haber farmacias cercanas devuelve esto
-    return JsonResponse(json_response, safe=False)
+        return JsonResponse([{'local_id':'-1'}], safe=False) # En caso de no haber farmacias cercanas devuelve esto
+
+    # return JsonResponse(farmacias_cercanas, safe=False)
