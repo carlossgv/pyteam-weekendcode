458e83cfd91b84fd52055b61e35452d9d8d4cb4f
Carlos Gonzalez <carlossgv@gmail.com>
2021-01-17 16:21:47 -0300

    fetch todo implementado con delay

--- pyteam/farmacias/views.py
+++ pyteam/farmacias/views.py
@@ -8,46 +8,59 @@ URL = "https://farmanet.minsal.cl/maps/index.php/ws/getLocalesTurnos"
 
 # Create your views here.
 
+
 class TestView(TemplateView):
     template_name = "farmacias/test.html"
 
-def encuentra_farmacias_cercanas(request, usuario_latitud = None, usuario_longitud = None):
 
-    if usuario_latitud == None or usuario_longitud == None:
-        raise Http404("Algo ha fallado con respecto a la latitud o longitud de la ubicacion del usuario")
+def encuentra_farmacias_cercanas(request, direccion):
 
-    usuario_latitud = float(usuario_latitud)
-    usuario_longitud = float(usuario_longitud)
-    
-    farmacias_cercanas = devuelve_farmacias_cercanas(usuario_latitud, usuario_longitud, URL)
-    
-    if farmacias_cercanas:
-        return JsonResponse(farmacias_cercanas, safe=False)        
+    if "son_coordenadas" in direccion:
+        direccion = direccion.split("_")
+        usuario_latitud = float(direccion[0])
+        usuario_longitud = float(direccion[1])
     else:
-        return JsonResponse([{'local_id':'-1'}], safe=False) # En caso de no haber farmacias cercanas devuelve esto
+        usuario_latitud, usuario_longitud = devuelve_latlng_usuario(direccion)
+
+    # if usuario_latitud == None or usuario_longitud == None:
+    # raise Http404(
+    #     "Algo ha fallado con respecto a la latitud o longitud de la ubicacion del usuario"
+    # )
+
+    farmacias_cercanas = devuelve_farmacias_cercanas(
+        usuario_latitud, usuario_longitud, URL
+    )
+
+    if farmacias_cercanas:
+        return JsonResponse(farmacias_cercanas, safe=False)
+    else:
+        return JsonResponse(
+            [{"local_id": "-1"}], safe=False
+        )  # En caso de no haber farmacias cercanas devuelve esto
 
     # return JsonResponse(farmacias_cercanas, safe=False)
 
-def encuentra_farmacias_by_direccion(request):
 
-    direccion = request.GET.get('direccion')
+# def encuentra_farmacias_by_direccion(request):
 
-    if direccion:
-        
-        usuario_latitud, usuario_longitud = devuelve_latlng_usuario(direccion)
+#     direccion = request.GET.get("direccion")
 
-        # En caso de que la direccion no haya sido encontrada devuelve lo siguiente
-        if usuario_latitud == None:
-            return JsonResponse([{'local_id':'-2'}], safe=False) 
+#     if direccion:
 
-        farmacias_cercanas = devuelve_farmacias_cercanas(usuario_latitud, usuario_longitud, URL)
+#         usuario_latitud, usuario_longitud = devuelve_latlng_usuario(direccion)
 
-        if farmacias_cercanas:
-            return JsonResponse(farmacias_cercanas, safe=False)        
-        else:
-            return JsonResponse([{'local_id':'-1'}], safe=False)
-        
-    else:
-        return JsonResponse([{'local_id':'-2'}], safe=False)
+#         # En caso de que la direccion no haya sido encontrada devuelve lo siguiente
+#         if usuario_latitud == None:
+#             return JsonResponse([{"local_id": "-2"}], safe=False)
 
-    
+#         farmacias_cercanas = devuelve_farmacias_cercanas(
+#             usuario_latitud, usuario_longitud, URL
+#         )
+
+#         if farmacias_cercanas:
+#             return JsonResponse(farmacias_cercanas, safe=False)
+#         else:
+#             return JsonResponse([{"local_id": "-1"}], safe=False)
+
+#     else:
+#         return JsonResponse([{"local_id": "-2"}], safe=False)
